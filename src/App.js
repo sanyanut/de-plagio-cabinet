aca4bff6956aba66e30c00e90b57057e72716b48
Alexander Sorochinskiy <alexander.sorochinskiy@gmail.com>
2022-05-26 14:52:39 +0300

    hash router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import {HashRouter} from "react-router-dom";
 
 import PlagiarismChecker from "./pages/plagiarism-checker/plagiarism-checker.component";
 
 function App() {
   return (
     <div className="App">
-      <PlagiarismChecker />
+        <HashRouter basename={process.env.PUBLIC_URL}>
+            <PlagiarismChecker />
+        </HashRouter>
     </div>
   );
 }
